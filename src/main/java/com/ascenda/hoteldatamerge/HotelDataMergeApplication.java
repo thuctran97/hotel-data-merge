af78c3471ad592c9dbee7a7c4d0f4b457867e667
Icarus <thucctin@gmail.com>
2024-02-02 14:58:04 +0700

    Init

---
+++ src/main/java/com/ascenda/hoteldatamerge/HotelDataMergeApplication.java
@@ -0,0 +1,13 @@
+package com.ascenda.hoteldatamerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelDataMergeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelDataMergeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ascenda/hoteldatamerge/HotelDataMergeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascenda.hoteldatamerge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelDataMergeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
